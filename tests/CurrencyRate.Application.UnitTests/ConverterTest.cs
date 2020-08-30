37a33472f1423384ca486c51f250bc2abc3d361f
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-08-30 21:13:23 +0300

    изменил тесты для конвертора валют

--- tests/CurrencyRate.Application.UnitTests/ConverterTest.cs
+++ tests/CurrencyRate.Application.UnitTests/ConverterTest.cs
@@ -4,25 +4,41 @@ using CurrencyRate.Application.Converter;
 namespace CurrencyRate.Application.UnitTests
 {
     [TestFixture]
-    public class ConverterTest
+    public class CalculateAmountTest
     {
         [Test]
-        public void CalculateAmountTest()
+        public void WithPrimesNumbers()
         {
             var converter = new Converter.Converter();
             var result = converter.CalculateAmount(74, 5, 2);
             Assert.That(result, Is.EqualTo(29.6m));
+        }
 
-            result = converter.CalculateAmount(2, 80, 40.2m);
+        public void WithFractionalNumbers()
+        {
+            var converter = new Converter.Converter();
+            var result = converter.CalculateAmount(2, 80, 40.2m);
             Assert.That(result, Is.EqualTo(1.005m));
+        }
 
-            result = converter.CalculateAmount(73.80m, 12, 0);
+        public void WithZeroValue()
+        {
+            var converter = new Converter.Converter();
+            var result = converter.CalculateAmount(73.80m, 12, 0);
             Assert.That(result, Is.EqualTo(0));
+        }
 
-            result = converter.CalculateAmount(0, 1, 1);
+        public void ZeroRateAvailableCurrency()
+        {
+            var converter = new Converter.Converter();
+            var result = converter.CalculateAmount(0, 1, 1);
             Assert.That(result, Is.EqualTo(0));
+        }
 
-            result = converter.CalculateAmount(1, 0, 1);
+        public void ZeroRateDesiredCurrency()
+        {
+            var converter = new Converter.Converter();
+            var result = converter.CalculateAmount(1, 0, 1);
             Assert.That(result, Is.EqualTo(0));
         }
     }
